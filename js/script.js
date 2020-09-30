adfd92e078ab0acf38f418ac8f73e70e12460343
unknown <kadamsarvesh1905@gmail.com>
2020-10-01 00:20:33 +0530

    about section

--- js/script.js
+++ js/script.js
@@ -5,9 +5,13 @@ document.querySelector('.toggle-btn').addEventListener('click',()=>{
     document.querySelector('.header--main-nav').classList.toggle('show-links');
 })
 
-// document.addEventListener('scroll',(event)=>{
-// 	var navbar = document.querySelector('nav');
-// 	navbar.classList.toggle("sticky",document.body.scrollTop > 670 || document.documentElement.scrollTop > 670 );
-// 	console.log(event.scrollTop);
-// })
+document.addEventListener('scroll',(event)=>{
+	var navbar = document.querySelector('nav');
+	navbar.classList.toggle("sticky",document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 );
+	console.log(event.scrollTop);
+})
 
+
+document.querySelector('.nav-opt').addEventListener('click',()=>{
+    document.querySelector('.toggle-btn').click()
+})
